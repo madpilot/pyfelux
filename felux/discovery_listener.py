87f0b2e7a2031fd4483ca7b249cfbdfb320d0687
Myles Eftos <myles@madpilot.com.au>
2020-06-12 20:26:14 +1000

    Use the first IP address that comes from discovery

--- felux/discovery_listener.py
+++ felux/discovery_listener.py
@@ -7,17 +7,17 @@ class DiscoveryListener:
 
     def add_service(self, zeroconf, type, name):
         info = zeroconf.get_service_info(type, name)
-        properties = info.properties.items()
+        if info:
+            properties = info.properties.items()
+            ip_address = inet_ntoa(info.addresses[0])
+            host_name = info.server
+            id = self._get_property(properties, "id")
+            id = int(str(id), 16)
+            device_name = self._get_property(properties, "device_name")
+            number_of_leds = self._get_property(properties, "leds")
 
-        ip_address = inet_ntoa(info.address)
-        host_name = info.server
-        id = self._get_property(properties, "id")
-        id = int(str(id), 16)
-        device_name = self._get_property(properties, "device_name")
-        number_of_leds = self._get_property(properties, "leds")
-
-        device = felux.Device(id, ip_address, host_name, device_name, number_of_leds)
-        self._manager.add_device(device)
+            device = felux.Device(id, ip_address, host_name, device_name, number_of_leds)
+            self._manager.add_device(device)
 
     def remove_server(self, zeroconf, type, name):
         info = zeroconf.get_service_info(type, name)
